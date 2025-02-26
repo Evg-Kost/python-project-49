a6d45bf382c47547093e00260b202f7a505c1236
Evgeny Kostolomov <e.kostolomov@bk.ru>
2025-02-26 20:39:52 +0300

    change brain-even

---
+++ brain_games/even_games.py
@@ -0,0 +1,23 @@
+import random
+
+
+def even_game(name):
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count = 0
+    while count < 3:
+        random_number = random.randint(1, 100)
+        print(f'Question: {random_number}')
+        if random_number % 2 == 0:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
+        read = input('Your answer:')
+        if read == correct_answer:
+            print('Correct!')
+            count += 1
+        else:
+            print(f"'{read}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            break
+    if count == 3:
+        print(f"Congratulations, {name}!")
