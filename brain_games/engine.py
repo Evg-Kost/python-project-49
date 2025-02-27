c89c207b99a7426cb2468ea557a971e0292e27e5
Evgeny Kostolomov <e.kostolomov@bk.ru>
2025-02-27 19:29:19 +0300

    change engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,8 +1,9 @@
-def check_answer(correct_answer, answer, name):
-        if correct_answer == answer:
-            print('Correct!')
-            return True
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return False
+def check_answer(correct_answer, name):
+    answer = input('Your answer:')
+    if correct_answer == answer:
+        print('Correct!')
+        return True
+    else:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+        print(f"Let's try again, {name}!")
+        return False
